7920f8799a5de3cb49a41efbe887086e6f11ad53
Siddhanth Prafulla Pathak <sidpathak07@gmail.com>
2026-06-03 16:22:56 +0530

    Login, Signup and Redux Appstore

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,76 @@
 import Header from "./Header";
-import {useState} from "react";
+import {useRef, useState} from "react";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import {addUser} from "../utils/userSlice"
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 const Login = () =>{
     const [isSignIn, setIsSignIn] = useState(true);
+    const dispatch = useDispatch();
+    const email = useRef(null);
+    const password = useRef(null);
+    const [emailError,setEmailError] = useState("");
+    const [passwordError,setPasswordError] = useState("");
+    const [firebaseError, setFirebaseError] = useState("");
+    const isValidEmail = (email)=>{
+        let isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+        if(!isEmailValid) return "Invalid email";
+        return true;
+    }
+    const isValidPassword = (password) =>{
+        let isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z\d]).{8,}$/.test(password)
+        if(!isPasswordValid) return "Enter valid password";
+        return true;
+    }
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setEmailError("");
+        setPasswordError("");
+        const emailValue = email.current.value;
+        const passwordValue = password.current.value;
+        console.log("Email:", emailValue);
+        console.log("Password:", passwordValue);
+        const checkEmailValidity = isValidEmail(emailValue);
+        const checkPasswordValidity = isValidPassword(passwordValue);
+        
+        setEmailError(checkEmailValidity);
+        setPasswordError(checkPasswordValidity);
+
+        if(checkEmailValidity === true && checkPasswordValidity === true){
+            if(isSignIn){
+                signInWithEmailAndPassword(auth, emailValue, passwordValue)
+                .then((userCred)=>{
+                    console.log(userCred);
+                    const user = userCred.user;
+                    setFirebaseError("");
+                    dispatch(addUser({
+                        email:user.email,
+                        uid:user.uid,
+                        accessToken:user.stsTokenManager.accessToken,
+                        refreshToken:user.stsTokenManager.refreshToken
+                    }));
+                }).catch(error=>{
+                    console.log(error.message)
+                    setFirebaseError(error.message);
+                })
+            }else{
+                createUserWithEmailAndPassword(auth, emailValue, passwordValue)
+                .then((userCred)=>{
+                    console.log(userCred);
+                    const user = userCred.user;
+                    setFirebaseError("");
+                    dispatch(addUser({
+                        email:user.email,
+                        uid:user.uid,
+                        accessToken:user.stsTokenManager.accessToken
+                    }));
+                }).catch(error=>{
+                    console.log(error.message)
+                    setFirebaseError(error.message);
+                })
+            }
+        }
+    };
     return(
         <div
             className="min-h-screen flex flex-col bg-cover bg-center"
@@ -9,12 +78,15 @@ const Login = () =>{
         >
             <Header />
             <div className="flex-1 flex items-center justify-center px-4 py-8">
-                <form className="w-1/3 rounded-lg bg-black bg-opacity-70 flex flex-col gap-6 py-12 px-4 rounded-lg">
+                <form className="w-1/3 rounded-lg bg-black bg-opacity-70 flex flex-col gap-6 py-12 px-4">
                     <h3 className="text-white font-bold text-2xl py-2 w-2/3 mx-auto">{isSignIn?"Sign In":"Sign Up"}</h3>
                     <input className={`w-2/3 mx-auto px-3 py-2 rounded bg-gray-200 ${isSignIn ? 'hidden' : 'visible'}`} placeholder="Name" type="text" />
-                    <input className="w-2/3 mx-auto px-3 py-2 rounded bg-gray-200" placeholder="Email" type="email" />
-                    <input className="w-2/3 mx-auto px-3 py-2 rounded bg-gray-200" placeholder="Password" type="password" />
-                    <button className="w-2/3 mx-auto px-3 py-2 bg-red-600 text-white rounded font-semibold" type="submit">{isSignIn?"Sign In":"Sign Up"}</button>
+                    <input ref={email} className="w-2/3 mx-auto px-3 py-2 rounded bg-gray-200" placeholder="Email" type="email" />
+                    {emailError!==true && <p className="w-2/3 mx-auto text-red-500">{emailError}</p>}
+                    <input ref={password} className="w-2/3 mx-auto px-3 py-2 rounded bg-gray-200" placeholder="Password" type="password" />
+                    {passwordError!==true && <p className="w-2/3 mx-auto text-red-500">{passwordError}</p>}
+                    {firebaseError && <p className="w-2/3 mx-auto text-red-500">{firebaseError}</p>}
+                    <button onClick={(e)=>handleSubmit(e)} className="w-2/3 mx-auto px-3 py-2 bg-red-600 text-white rounded font-semibold" type="submit">{isSignIn?"Sign In":"Sign Up"}</button>
                     <p className={`py-2 w-2/3 mx-auto text-gray-400 ${isSignIn ? 'visible' : 'hidden'}`}>Dont have account?<span className="text-gray-200 font-bold text-l hover:cursor-pointer" onClick={()=>setIsSignIn(!isSignIn)}>Sign Up</span></p>
                     <p className={`py-2 w-2/3 mx-auto text-gray-400 ${isSignIn ? 'hidden' : 'visible'}`}>Already have account?<span className="text-gray-200 font-bold text-l hover:cursor-pointer" onClick={()=>setIsSignIn(!isSignIn)}>Sign In</span></p>
                 </form>
---
+++ src/utils/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCP4XCeGDzeIxUui0Z-2YKsSttRbD9w7hg",
+  authDomain: "netflixgpt-fbff3.firebaseapp.com",
+  projectId: "netflixgpt-fbff3",
+  storageBucket: "netflixgpt-fbff3.firebasestorage.app",
+  messagingSenderId: "518735065094",
+  appId: "1:518735065094:web:5fe156ccaf5ce0b8e49387",
+  measurementId: "G-6L2D6PZGN0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { auth, app };
